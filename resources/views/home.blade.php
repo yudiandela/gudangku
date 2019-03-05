2397da1e21eee0d9232b5a1e50749fbd7a992eac
Yudi Andela <yudhi.andhela@gmail.com>
2019-03-05 23:22:17 +0700

    Setting Halaman Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.main')
+
+@section('title', 'Homepage')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Dashboard</h6>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,7 +27,7 @@
         <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('index') }}">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
@@ -39,7 +39,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('index') }}">
+                <a class="nav-link" href="{{ route('home') }}">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Home</span>
                 </a>
